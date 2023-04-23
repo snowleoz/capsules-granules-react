e7fd7e18318340ce03b2e9f483e0b2557c6c6731
zhangrui <393394595@qq.com>
2023-04-24 01:13:24 +0800

    style: 格式化代码

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,51 +3,55 @@ import React, { createElement, Dispatch } from 'react'
 import { ReducerPayload } from '../src/components/Updater'
 
 export interface IProps {
-  /** 组件描述 */
-  config: {
-    type: string
-    props?: Record<string, any>
-    particleOption?: Record<string, any>
-  } & Description
-  /** 是否深拷贝描述 */
-  cloneDeepConfig?: boolean
-  /** 组件注册 */
-  register: Array<{
-    type: string
-    component: ReactCreateElementCmpt
-  }>
-  /** 组件渲染前加载的预置组件 loading */
-  loading?: React.ReactElement
+	/** 组件描述 */
+	config: {
+		type: string
+		props?: Record<string, any>
+		particleOption?: Record<string, any>
+	} & Description
+	/** 是否深拷贝描述 */
+	cloneDeepConfig?: boolean
+	/** 组件注册 */
+	register: Array<{
+		type: string
+		component: ReactCreateElementCmpt
+	}>
+	/** 组件渲染前加载的预置组件 loading */
+	loading?: React.ReactElement
 }
 
 export type ReactCreateElements = ReactCreateElement[] | null
 export type ReactCreateElement = ReturnType<typeof createElement> | null
 export type ReactCreateElementCmpt = Parameters<typeof createElement>[0]
 export type RegisterRef = {
-  register: IProps['register']
-  registerMap: Record<string, Parameters<typeof createElement>[0]>
+	register: IProps['register']
+	registerMap: Record<string, Parameters<typeof createElement>[0]>
 }
 export type ReactElementsRef = {
-  children: Record<string, Array<ReturnType<typeof createElement>>>
-  element: Record<string, ReturnType<typeof createElement>>
+	children: Record<string, Array<ReturnType<typeof createElement>>>
+	element: Record<string, ReturnType<typeof createElement>>
 }
 
 export type reactUpdateQuotoRef = Record<
-  string,
-  {
-    data: ReducerPayload
-  }
+	string,
+	{
+		data: ReducerPayload
+	}
 >
 
 export type particleDispatchRef = Record<string, Dispatch<ReducerPayload>>
 
 export type ImperativeRef = RegisterRef & {
-  getParticle: () => ReturnType<Particle['getParticle']> | undefined
-  getItem: (keys?: string[], dataType?: 'object' | 'array') => ReturnType<Particle['getItem']>
-  append: (key: string, config: IProps['config'] | IProps['config'][], order?: number | undefined) => ReturnType<Particle['append']>
-  remove: (keys: string[]) => ReturnType<Particle['remove']>
-  setItem: (key: string, data: Record<string, any>) => ReturnType<Particle['setItem']>
-  replace: (key: string, config: IProps['config']) => ReturnType<Particle['replace']>
+	getParticle: () => ReturnType<Particle['getParticle']> | undefined
+	getItem: (keys?: string[], dataType?: 'object' | 'array') => ReturnType<Particle['getItem']>
+	append: (
+		key: string,
+		config: IProps['config'] | IProps['config'][],
+		order?: number | undefined
+	) => ReturnType<Particle['append']>
+	remove: (keys: string[]) => ReturnType<Particle['remove']>
+	setItem: (key: string, data: Record<string, any>) => ReturnType<Particle['setItem']>
+	replace: (key: string, config: IProps['config']) => ReturnType<Particle['replace']>
 }
 
 const ParticleReact: React.ForwardRefExoticComponent<IProps & React.RefAttributes<ImperativeRef>>
--- vite.config.ts
+++ vite.config.ts
@@ -7,34 +7,34 @@ const typePath = resolve(process.cwd(), './typings/index.d.ts')
 const distPath = resolve(process.cwd(), './dist')
 
 export default defineConfig({
-  plugins: [
-    reactPlugin(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: typePath,
-          dest: distPath
-        }
-      ]
-    })
-  ],
-  build: {
-    target: ['es2015'],
-    lib: {
-      entry: './src/index.tsx',
-      formats: ['cjs', 'es', 'umd'],
-      name: 'capsule-particle-react',
-      fileName: 'index'
-    },
-    rollupOptions: {
-      external: ['lodash-es', 'react', 'react-dom'],
-      output: {
-        globals: {
-          'lodash-es': '_',
-          react: 'react',
-          'react-dom': 'reactDom'
-        }
-      }
-    }
-  }
+	plugins: [
+		reactPlugin(),
+		viteStaticCopy({
+			targets: [
+				{
+					src: typePath,
+					dest: distPath
+				}
+			]
+		})
+	],
+	build: {
+		target: ['es2015'],
+		lib: {
+			entry: './src/index.tsx',
+			formats: ['cjs', 'es', 'umd'],
+			name: 'capsule-particle-react',
+			fileName: 'index'
+		},
+		rollupOptions: {
+			external: ['lodash-es', 'react', 'react-dom'],
+			output: {
+				globals: {
+					'lodash-es': '_',
+					react: 'react',
+					'react-dom': 'reactDom'
+				}
+			}
+		}
+	}
 })
