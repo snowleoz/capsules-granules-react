0947a67006a695cdd6b6df667b3c3ab4cbaa80ce
zhangrui <393394595@qq.com>
2023-04-30 21:19:42 +0800

    feat: 增加react树组件的解析；

--- src2/index.tsx
+++ src2/index.tsx
@@ -1,26 +1,48 @@
 import React, { useMemo, Fragment, useState } from 'react'
 import Particle from 'capsule-particle'
-import { useCache, UseCacheReturn } from './hooks'
+import { useCache } from './hooks'
 import { initRegistry, controller } from './utils'
-import type { IParticleReactProps } from '../typings'
+import type { IParticleReactProps, RegistryItem, ParticleReactItem, ReactElements } from '../typings'
+
+export type ParticleDataRef = {
+	/** 组件注册信息映射表 */
+	registeredMap?: Record<string, RegistryItem>
+	/** 组件注册表 */
+	registeredCmptMap?: Record<string, RegistryItem['component']>
+	/** 对象树实例 */
+	particleEntity?: Particle
+	/** 当前的组件树 */
+	reactTree: ReactElements[]
+	/** 当前打平的组件树 */
+	flatReactTree: Record<string, ReactElements>
+	/** 每个树节点的children容器 */
+	reactTreeChildren: Record<string, ReactElements[]>
+	/** 每个组件的更新器 */
+	reactUpdaters: Record<string, React.Dispatch<any>>
+}
 
 const ParticleReact = (props: IParticleReactProps) => {
 	const { registry, feedback, configs } = props
+
 	/** 组件刷新器 */
-	const [, update] = useState(0)
+	const [updateCount, update] = useState(0)
 
 	/** 缓存数据 */
-	const cacheDataRef = useCache({
-		/** 组件注册表 */
+	const particleDataRef = useCache<ParticleDataRef>({
+		/** 组件注册信息映射表 */
 		registeredMap: undefined,
+		/** 组件注册表 */
+		registeredCmptMap: undefined,
 		/** 对象树实例 */
 		particleEntity: undefined,
 		/** 当前的组件树 */
-		reactTree: undefined,
+		reactTree: [],
 		/** 当前打平的组件树 */
-		flatReactTree: undefined,
+		flatReactTree: {},
 		/** 每个树节点的children容器 */
-		reactTreeChildren: undefined
+		reactTreeChildren: {},
+		/** 每个组件的更新器 */
+		reactUpdaters: {}
 	})
 
 	/**
@@ -28,14 +50,36 @@ const ParticleReact = (props: IParticleReactProps) => {
 	 * 仅第一次渲染时接受configs，后续需通过api来对组件树进行增删改查
 	 */
 	useMemo(() => {
-		cacheDataRef.setCache({
-			registeredMap: initRegistry(registry),
-			particleEntity: new Particle(configs, (configItem) => {
-				controller(configItem, cacheDataRef as UseCacheReturn, update)
+		console.time('parse')
+		const registryInfo = initRegistry(registry)
+		if (registryInfo) {
+			const { registeredMap, registeredCmptMap } = registryInfo
+			particleDataRef.setCache({
+				registeredMap,
+				registeredCmptMap
 			})
-		})
+			particleDataRef.setCache({
+				/**
+				 * TODO Particle 增加泛型
+				 * 需要将注册完成后才开始解析配置
+				 */
+				particleEntity: new Particle(configs, (configItem) => {
+					controller(configItem as unknown as ParticleReactItem, registeredCmptMap, particleDataRef)
+				})
+			})
+			update((count) => ++count)
+		} else {
+			/** TODO: 组件使用Error Boundaries */
+			throw new Error('Missing valid component registration information')
+		}
+		console.timeEnd('parse')
 	}, [])
-	return <Fragment>{cacheDataRef.getCache('reactTree') ? <div>React</div> : feedback || null}</Fragment>
+
+	const ReactTree = useMemo(() => {
+		return particleDataRef.getCache('reactTree')
+	}, [updateCount])
+
+	return <Fragment>{ReactTree.length ? ReactTree : feedback || null}</Fragment>
 }
 
 export default ParticleReact
--- src2/utils/controller.ts
+++ src2/utils/controller.ts
@@ -1,12 +1,61 @@
-import { ParticleItem } from 'capsule-particle'
-import { UseCacheReturn } from '../hooks'
+import { createElement } from 'react'
+import { Updater } from '../components'
+import { PARTICLE_TOP } from 'capsule-particle'
+import type { ParticleDataRef } from '../'
+import type { UseCacheReturn } from '../hooks'
+import { Error } from '../components'
+import type { ParticleReactItem, ReactElements } from '../../typings'
 
 export function controller(
-	configItem: ParticleItem,
-	cacheDataRef: UseCacheReturn,
-	update: React.Dispatch<React.SetStateAction<number>>
+	configItem: ParticleReactItem,
+	registeredCmptMap: ParticleDataRef['registeredCmptMap'],
+	cacheDataRef: UseCacheReturn<ParticleDataRef>
 ) {
-	console.log('configItem: ', configItem)
-	cacheDataRef
-	update
+	const { type, key, props = {}, __particle } = configItem
+	const { parent } = __particle
+	if (type && registeredCmptMap) {
+		let Component = registeredCmptMap[type]
+		if (!Component) {
+			Component = Error
+			console.error(
+				'Component not registered, skipping rendering, config is ',
+				JSON.stringify(configItem),
+				'.Using the Error component instead'
+			)
+		}
+		const reactUpdaters = cacheDataRef.getCache('reactUpdaters')
+		/** 保存当前组件children的应用，方便之后将子级的信息推入 */
+		const reactTreeChildren: ReactElements[] = []
+		const ParticleCmpt = createElement(Updater, {
+			config: configItem,
+			render: Component,
+			renderProps: props,
+			renderChildren: reactTreeChildren,
+			reactUpdaters,
+			key: `${key}-updater`
+		})
+		if (parent === PARTICLE_TOP) {
+			const reactTree = cacheDataRef.getCache('reactTree')
+			reactTree.push(ParticleCmpt)
+		} else {
+			const parentChildren = cacheDataRef.getCache('reactTreeChildren')[parent]!
+			parentChildren.push(ParticleCmpt)
+		}
+		/** 将数据存储到缓存中 */
+		cacheDataRef.setCache(
+			{
+				reactTreeChildren: {
+					[key]: reactTreeChildren
+				},
+				flatReactTree: {
+					[key]: ParticleCmpt
+				}
+			},
+			{
+				merge: true
+			}
+		)
+	} else {
+		console.error('Missing component information, current component configuration is ', JSON.stringify(configItem))
+	}
 }
