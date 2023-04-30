5ba020ae87811fb32d5ec4215c102d350db4a7e9
zhangrui <393394595@qq.com>
2023-04-30 21:18:26 +0800

    fix: 增加设置缓存时，可选深度合并；

--- src2/hooks/cache.ts
+++ src2/hooks/cache.ts
@@ -1,4 +1,5 @@
 import { useRef, useCallback } from 'react'
+import { merge } from 'lodash-es'
 
 export type UseCacheReturn<T extends object> = ReturnType<typeof useCache<T>>
 
@@ -6,9 +7,22 @@ export type UseCacheReturn<T extends object> = ReturnType<typeof useCache<T>>
 const useCache = <T extends object>(data: T) => {
 	const cacheData = useRef<T>(data)
 
-	const setCache = useCallback((data: T | object) => {
-		Object.assign(cacheData.current, data)
-	}, [])
+	const setCache = useCallback(
+		(
+			data: T | object,
+			options?: {
+				merge?: boolean
+			}
+		) => {
+			const { merge: mergeOption } = options || {}
+			if (mergeOption) {
+				merge(cacheData.current, data)
+			} else {
+				Object.assign(cacheData.current, data)
+			}
+		},
+		[]
+	)
 
 	const getCache = useCallback(<K extends keyof T>(key: K | (string & object)): T[K] => {
 		return cacheData.current[key]
@@ -16,7 +30,8 @@ const useCache = <T extends object>(data: T) => {
 
 	return {
 		setCache,
-		getCache
+		getCache,
+		cacheData
 	}
 }
 
