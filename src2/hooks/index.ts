b34d51ac9e02b08b4b18219a938524e37f5a7e4f
zhangrui <393394595@qq.com>
2023-05-09 01:05:24 +0800

    chore: 去除冗余的hook

--- src2/hooks/index.ts
+++ src2/hooks/index.ts
@@ -1,5 +1,3 @@
-import useCache from './cache'
-import type { UseCacheReturn } from './cache'
 import useImperative from './imperativeHandle'
 
-export { useCache, UseCacheReturn, useImperative }
+export { useImperative }
