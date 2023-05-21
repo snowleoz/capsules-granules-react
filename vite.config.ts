bd87db32a417b5ee5fb014fff4a8bc2f5e793126
zhangrui <393394595@qq.com>
2023-05-22 00:58:50 +0800

    fix: 调整类型文件的生成逻辑；

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,8 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 import reactPlugin from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
-
-const typePath = resolve(process.cwd(), './typings/index.d.ts')
-const distPath = resolve(process.cwd(), './dist')
 
 export default defineConfig({
-	plugins: [
-		reactPlugin(),
-		viteStaticCopy({
-			targets: [
-				{
-					src: typePath,
-					dest: distPath
-				}
-			]
-		})
-	],
+	plugins: [reactPlugin()],
 	build: {
 		target: ['es2015'],
 		lib: {
